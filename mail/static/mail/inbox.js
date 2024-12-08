4a0dd2187b28fb310cc3ded6542f6c876d0cef50
lucashurt <lucashurtado814@gmail.com>
2024-12-08 15:36:48 -0500

    added inbox functionality that displays all the emails in that specific inbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -31,6 +31,45 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(`/emails/${mailbox}`)
+    .then(res => res.json())
+      .then(email => {
+        email.forEach(individual_email => {
+          const element = document.createElement('div');
+          element.className = "row";
+          element.style.border="1px solid black"
+          element.style.justifyContent="space-between";
+
+          const sender = document.createElement('div');
+          sender.innerHTML = individual_email.sender;
+          sender.style.fontWeight="bold";
+          sender.style.marginRight="5%";
+
+          const subject = document.createElement('div');
+          subject.innerHTML = individual_email.subject;
+          subject.style.border="1px solid black"
+          subject.style.textAlign="center"
+
+          const left_side = document.createElement('div');
+          left_side.className = "row";
+          left_side.append(sender);
+          left_side.append(subject);
+          left_side.style.paddingLeft="2%";
+          left_side.style.width="70%"
+
+          const timestamp = document.createElement('div');
+          timestamp.innerHTML = individual_email.timestamp;
+          timestamp.style.color = "grey";
+
+
+          element.append(left_side)
+          element.append(timestamp)
+
+          element.addEventListener('click', () => console.log("clicked"));
+          document.querySelector('#emails-view').append(element);
+        })
+      })
 }
 
 function send_email(event) {
@@ -46,6 +85,7 @@ function send_email(event) {
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    load_mailbox('sent');
   })
 }
 
